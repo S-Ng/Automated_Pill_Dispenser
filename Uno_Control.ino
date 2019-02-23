7c4ee60f66055bc350ba4047567cb5c8fa557e60
Simon Ng <33298951+S-Ng@users.noreply.github.com>
2019-02-23 13:56:58 -0800

    Added Compartment and Elevator Stepper Codes
    Might need to edit compartment code to move correct number of steps (it moves fastest direction, but maybe not the right number of steps)

--- Uno_Control.ino
+++ Uno_Control.ino
@@ -1,9 +1,87 @@
-void setup() {
-  // put your setup code here, to run once:
-  // Let's add stuff
+#include <Stepper.h>
+
+#define STEPS 200
+
+// create an instance of the stepper class, specifying the number of steps of the motor and the pins it's attached to
+Stepper Compartment_stepper(STEPS, 4, 5, 6, 7);
+Stepper Elevator_stepper(STEPS, 8, 9, 10, 12);
+
+
+int CurrentLoc = 6;//value 1-8. Need a way to initialize this value. Could link to LCD screen for user calibration
+
+void setup() { // put your setup code here, to run once:
+  Serial.begin(9600); // baud rate
+  
+  // set the speed of the motor in RPMs
+  Compartment_stepper.setSpeed(10);
+  Elevator_stepper.setSpeed(5);
+  bool Move_Elevator = true;
+  bool dispensed = false;
+  bool IR = false; // continue running elevator code
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() {  // put your main code here, to run repeatedly:
+  
+  // Compartment_Stepper Code
+  bool dispense = true;
+  if (dispense == true){
+    int NewLoc = 8; // need new location input from somewhere
+    Move_Compartment_Stepper(NewLoc);
 
+   // Elevator_Stepper Code
+   if (Move_Elevator == true){ // run code to capture pill
+      
+      float Dist = 20.6; // distance in mm to extend pill elevator into compartment
+      
+      while (dispensed == false){ // rerun code to capture pill, extending pill compartment slighly further
+        Move_Elevator_Stepper(Dist);
+        //Dist = Dist + 1; // go 1mm further each repetition
+        
+          if (IR == true){ // stop repeating dispensing mechanism
+              dispensed = true;
+          }
+     }
+  }
+}
+
+// Rotate to Correct Pill Compartment
+void Move_Compartment_Stepper(int NewLoc){ 
+
+    int NewLocSteps = (NewLoc - 1) * 25; // convert current location into steps
+    int CurrentLocSteps = (CurrentLoc - 1) * 25; // convert current location into steps. 25 steps for 45 degrees
+
+    int NumSteps = CurrentLocSteps - NewLocSteps; // find num steps between current container and new container
+    
+    int Direction = 1; // default direction is clockwise (+1)
+
+    // Test whether counter clockwise is faster
+    if ((NumSteps >= 25 && NumSteps <= 100) || (NumSteps >= -175 && NumSteps < -100)){ // if 25 <= NumSteps <= 100 or if -175 <= NumSteps < -100
+      Direction = -1; // change direction to CCW (-1)
+    }
+
+    int Move = abs(NumSteps)*Direction; // move number of steps either CW or CCW
+    Serial.println(Move); // print movement
+    Compartment_stepper.step(Move); // turn stepper
+    delay(1000); // wait seconds
+
+    CurrentLoc = NewLoc; // set new location to current location once new location has been reached
+}
+
+// Move Pill Elevator
+void Move_Elevator_Stepper(float Dist){ 
+
+    float tot_Dist = Dist + 8.4; // 8.4 is distance until inner front edge of elevator is in line w inner wall of cylinder so any further movement exposes open elevator
+    int Move = tot_Dist / 0.2585834333; // 2.154mm/tooth turn / 8.33 steps / tooth turn = 0.2586 mm traveled / step. Need to check how this rounds
+    
+    Serial.println(Move); // print movement
+    Elevator_stepper.step(Move); // move pill elevator into compartment
+
+    Serial.println("Wait for pill to enter");
+    delay(2000); // wait seconds. Pill falls in during this time
+    
+    Serial.println(-Move); // print movement
+    Elevator_stepper.step(-Move); // move pill elevator out of compartment
+
+    Serial.println("Wait for before attempting again");
+    delay(3000); // wait 3 seconds. Pill enters chute during this time
 }
